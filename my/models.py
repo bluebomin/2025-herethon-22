d9ca9624049713fad123030121bb1f47e5311238
yijoo <761610@naver.com>
2025-07-10 00:46:12 +0900

    my1차

--- my/models.py
+++ my/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class MyProfile(models.Model):
+    user_name = models.CharField(max_length=10)
+    REGION_CHOICES =[        
+        ('seoul', '서울'),
+        ('gyeonggi', '경기/인천'),
+        ('busan', '부산/울산/경남'),
+        ('daegu', '대구/경북'),
+        ('gwangju', '광주/전남/전북'),
+        ('daejeon', '대전/충남/충북'),
+        ('gangwon', '강원'),
+        ('jeju', '제주'),
+    ]
+    desired_region = models.CharField(max_length=20, choices=REGION_CHOICES)
+
+    #보유역량 갯수 개발하기
+
+    career_break = models.IntegerField(max_length=5, null=True, blank=True)
+
+
+
--- my/views.py
+++ my/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .forms import ProfileForm
 # Create your views here.
+def myprofile(request):
+    profile_form = ProfileForm()
+    context = {
+        'profileform' : profile_form
+    }
+    return render(request, 'myprofile.html', context)
