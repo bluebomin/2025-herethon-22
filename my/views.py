6fd0a959c5427f78428342e29775384bfa01c444
yijoo <761610@naver.com>
2025-07-11 01:48:43 +0900

    my연동

--- my/views.py
+++ my/views.py
@@ -1,18 +1,23 @@
+# my/views.py (수정된 부분)
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from plan.models import Plan
-from jobs.models import Bookmark, JobPost
+from plan.models import Plan 
+from jobs.models import Bookmark, JobPost 
 from django.contrib.auth.models import User
-# Create your views here.
 
 @login_required
 def myprofile(request):
-    all_my_profile = Plan.objects.all()
-    
-    all_bookmark = Bookmark.objects.filter(user=request.user)
+    my_plan = None
+    try:
+        my_plan = request.user.plan 
+    except Plan.DoesNotExist:
+        pass
+
+    all_bookmark = Bookmark.objects.filter(user=request.user) #
 
     context = {
-        'plans':all_my_profile,
-        'bookmarks':all_bookmark}
+        'my_plan': my_plan,
+        'bookmarks': all_bookmark
+    }
     return render(request, 'mypage.html', context)
-
