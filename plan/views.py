0a1d490935c65e3c489af4673b1964674a08b04d
김보민 <157809900+bluebomin@users.noreply.github.com>
2025-07-11 16:27:00 +0900

    [FIX] Plan
    보유역량 여러개 선택가능하도록 수정

--- plan/views.py
+++ plan/views.py
@@ -18,12 +18,10 @@ def plan_create_or_update(request):
             new_plan = form.save(commit=False)
             new_plan.user = request.user
             new_plan.save()
-            # GET 파라미터로 값 전달
             params = urlencode({
                 'desired_job': new_plan.desired_job,
                 'desired_region': new_plan.desired_region,
                 'career_gap_years': new_plan.career_gap_years,
-                # 필요시 추가
             })
             url = reverse('plan:plan_recommend') + f'?{params}'
             return redirect(url)
@@ -33,9 +31,13 @@ def plan_create_or_update(request):
     return render(request, 'plan/plan_form.html', {'form': form})
 
 def plan_recommend(request):
+    plan = None
+    if request.user.is_authenticated:
+        try:
+            plan = request.user.plan
+        except Plan.DoesNotExist:
+            plan = None
     context = {
-        'desired_job': request.GET.get('desired_job', ''),
-        'desired_region': request.GET.get('desired_region', ''),
-        'career_gap_years': request.GET.get('career_gap_years', ''),
+        'plan': plan,
     }
     return render(request, 'plan/plan_recommend.html', context)
